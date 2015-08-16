1d5d1f183c96512b22889dfd1e61f39536fae6e3
David Négrier <d.negrier@thecodingmachine.com>
2015-08-16 18:14:20 +0200

    Adding the ability to add expressions in the PHP code returned

--- src/Interop/Container/Compiler/DefinitionInterface.php
+++ src/Interop/Container/Compiler/DefinitionInterface.php
@@ -11,16 +11,28 @@ interface DefinitionInterface
     /**
      * Returns a string of PHP code generating the container entry.
      *
-     * The PHP code MUST be a closure, and that closure MUST take one argument that is a
+     * The PHP code MUST be a closure or a PHP expression that evaluates to the value of the item.
+     *
+     * If the PHP code is a closure, then that closure MUST take one argument that is a
      * Interop\Container\ContainerInterface object.
      * The function MUST return the entry generated.
      *
      * For instance, this is a valid PHP string:
      *
-     * function(Interop\Container\ContainerInterface $container) {
+     * "function(Interop\Container\ContainerInterface $container) {
      *     $service = new MyService($container->get('my_dependency'));
      *     return $service;
-     * }
+     * }"
+     *
+     * If the PHP code is a PHP expression, then the PHP expression must evaluate to the value returned for the
+     * container entry.
+     *
+     * These are valid PHP expressions:
+     *
+     * "'localhost'" (a string)
+     * "CONST_VAR" (a constant)
+     * "array(42, 12)" (an array)
+     * "12 + 32" (any valid PHP statement that evaluates to something)
      *
      * @return string
      */
